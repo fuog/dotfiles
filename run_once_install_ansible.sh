9d6e4214c9774d37be1bfdb16d2d242cfd372ff5
Nathanael Fuog <nathanael@fuog.net>
2024-03-10 10:36:26 +0100

    Adding some scripts

--- run_once_install_ansible.sh
+++ run_once_install_ansible.sh
@@ -2,16 +2,15 @@
 # debug with : chezmoi state delete-bucket --bucket=scriptState
 OS=$(uname -s)
 ARCH=$(uname -m)
-if [[ "$OS" == "Darwin" && "$ARCH" == "arm64" ]]; then
-    echo "This is a Darwin arm64 system"
+if [[ "$OS" == "Linux" && "$ARCH" == "x86_64" ]]; then
+    echo "This is a Linux amd64 system"
     command -v apt-get >/dev/null 2>&1 || \
         { echo "This Linux is not supported without 'apt-get'!"; exit 1 ;}
-
+    # only stuff that can not be managed by ansible later on
     sudo apt-get install ansible -y
-    echo foo
 
-elif [[ "$OS" == "Linux" && "$ARCH" == "x86_64" ]]; then
-    echo "This is a Linux amd64 system"
+elif [[ "$OS" == "Darwin" && "$ARCH" == "arm64" ]]; then
+    echo "This is a Darwin arm64 system"
 
 else
     echo "This is no debian/x86 or Darwin/arm64!"
